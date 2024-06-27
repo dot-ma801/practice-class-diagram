61c82b1f8b858476be8025d028a383ae7ee87157
dot-ma801 <dot.ma801@gmail.com>
2024-06-27 23:24:22 +0900

    [add] lesson4: person class を実装

---
+++ lesson4/src/person.ts
@@ -0,0 +1,17 @@
+export class Person {
+    private id: number;
+    private name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+}
